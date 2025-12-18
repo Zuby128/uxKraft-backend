abbc8cf4cf1d4038d25d384f984f74f40080a5d0
zberber20 <zberber20@gmail.com>
2025-12-18 03:42:25 -0500

    bug fixed

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -3,6 +3,6 @@ config();
 
 export const appConfig = {
   nodeEnv: 'development',
-  port: parseInt(process.env.PORT as string, 10) || 3000,
+  host: process.env.NODE_ENV === 'production' ? '0.0.0.0' : 'localhost',
   apiPrefix: 'api/v1',
 };
